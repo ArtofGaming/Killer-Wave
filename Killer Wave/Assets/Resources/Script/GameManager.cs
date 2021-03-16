7403344698922f56bf59a4a55684020dac6d6f7f
ArtofGaming <turner4965@gmail.com>
2021-03-15 22:28:49 -0600

    67% chapter 2

---
+++ Killer Wave/Assets/Resources/Script/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraSetup();
+        LightSetup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CameraSetup()
+    {
+        GameObject gameCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        gameCamera.transform.position = new Vector3(0, 0, -300);
+        gameCamera.transform.eulerAngles = new Vector3(0, 0, 0);
+        gameCamera.GetComponent<Camera>().clearFlags = CameraClearFlags.SolidColor;
+        gameCamera.GetComponent<Camera>().backgroundColor = new Color32(0, 0, 0, 255);
+    }
+    void LightSetup()
+    {
+        GameObject dirLight = GameObject.Find("Directional Light");
+        dirLight.transform.eulerAngles = new Vector3(50, -30, 0);
+        dirLight.GetComponent<Light>().color = new Color(152, 204, 255, 255);
+    }
+}
---
+++ Killer Wave/Assets/Resources/Script/PlayerSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    SOActorModel actorModel;
+    GameObject playerShip;
+      // Start is called before the first frame update
+      void Start()
+    {
+        CreatePlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreatePlayer()
+    {
+        actorModel = Object.Instantiate(Resources.Load
+       ("Script/ScriptableObject/Player_Default"))
+           as SOActorModel;
+        playerShip = GameObject.Instantiate(actorModel.actor) as GameObject;
+        playerShip.GetComponent<Player>().ActorStats(actorModel);
+
+        
+    }
+}
